910ddd687c77c3e546d52d7287f98d220b650903
arielmacias <arielmacias.rz@gmail.com>
2026-01-07 22:09:40 -0600

    Add defaults for database connection

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,8 +2,8 @@ import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
